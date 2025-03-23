5d89db4e41e06f986ff71bcdc4ea02abef3edfdb
Mostafa Kabil <kabilmostafa060@gmail.com>
2025-03-24 01:36:10 +0200

    path to 4 pages

---
+++ src/Pages/htmlPage.js
@@ -0,0 +1,177 @@
+import React from 'react';
+
+const Html = () => {
+  return (
+    <>
+    <header className="hide-when-mobile-screen">
+      <h1>Web Devs</h1>
+      <ul className="flex">
+        <li className="main-list">
+          <a className="main-link" href="www">
+            HTML
+          </a>
+          <ul className="sub-ul">
+            <li>
+              <a className="sub-link" href="www">
+                Full course
+              </a>
+            </li>
+            <li>
+              <a className="sub-link" href="www">
+                Crash Course
+              </a>
+            </li>
+            <li>
+              <a className="sub-link" href="www">
+                learn 1hours
+              </a>
+            </li>
+          </ul>
+        </li>
+        <li className="main-list">
+          <a className="main-link" href="www">
+            CSS
+          </a>
+          <ul className="sub-ul">
+            <li>
+              <a className="sub-link" href="www">
+                Full course
+              </a>
+            </li>
+            <li>
+              <a className="sub-link" href="www">
+                Css Example
+              </a>
+            </li>
+            <li className="mini-project">
+              <a className="sub-link" href="www">
+                Mini Projects&nbsp; &nbsp; &nbsp; +
+              </a>
+              <ul className="sub-sub-ul">
+                <li>
+                  <a className="sub-link" href="www">
+                    project 1
+                  </a>
+                </li>
+                <li>
+                  <a className="sub-link" href="www">
+                    project 2
+                  </a>
+                </li>
+                <li>
+                  <a className="sub-link" href="www">
+                    project 3
+                  </a>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </li>
+        <li className="main-list">
+          <a className="main-link" href="www">
+            JavaScript
+          </a>
+          <ul className="sub-ul sub-link-js">
+            <li>
+              <a href="www">Comming soon🔥</a>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </header>
+    <header className="show-when-mobile-screen">
+      <h1>Web devs</h1>
+      <label htmlFor="bar">
+        <i className="fa-solid fa-bars" />
+      </label>
+      <input id="bar" type="checkbox" />
+      <div className="show-onclick">
+        <div className="main-div">
+          <label className="checkbox" htmlFor="html">
+            HTML <i className="fa-solid fa-plus" />
+          </label>
+          <input type="checkbox" id="html" />
+          <ul className="sub-div">
+            <li>
+              <a className="sub-link" href="www">
+                Full course
+              </a>
+            </li>
+            <li>
+              <a className="sub-link" href="www">
+                Crash Course
+              </a>
+            </li>
+            <li>
+              <a className="sub-link" href="www">
+                learn 1hours
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="main-div">
+          <label className="checkbox" htmlFor="css">
+            CSS <i className="fa-solid fa-plus" />
+          </label>
+          <input type="checkbox" id="css" />
+          <ul className="sub-div">
+            <li>
+              <a className="sub-link" href="www">
+                Full course
+              </a>
+            </li>
+            <li>
+              <a className="sub-link" href="www">
+                Css Example
+              </a>
+            </li>
+            <li>
+              <label htmlFor="mini">
+                Mini Projects <i className="fa-solid fa-plus" />
+              </label>
+              <input type="checkbox" id="mini" />
+              <ul className="sub-sub-div">
+                <li>
+                  <a className="sub-link" href="www">
+                    project 1
+                  </a>
+                </li>
+                <li>
+                  <a className="sub-link" href="www">
+                    project 2
+                  </a>
+                </li>
+                <li>
+                  <a className="sub-link" href="www">
+                    project 3
+                  </a>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+        <div className="main-div">
+          <label className="checkbox" htmlFor="js">
+            JavaScript <i className="fa-solid fa-plus" />
+          </label>
+          <input type="checkbox" id="js" />
+          <ul className="sub-div">
+            <li>
+              <a href="www">Comming soon🔥</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </header>
+    <main>
+    HTML Page
+    </main>
+    <footer>
+      Designed and Devoloped By Web devs
+      <span>🧡</span>
+    </footer>
+  </>
+  );
+}
+
+export default Html;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,45 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './Pages/home';
+import Html from './Pages/htmlPage';
+import Css from './Pages/cssPage';
+import JavaScript from './Pages/javascript';
+
+
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>,
+    errorElement: <h2 style={{color:"red" , textAlign:"center"}}>Not Found This Page</h2>,
+  },
+  {
+    path: "/html",
+    element: <Html/>,
+  },
+  {
+    path: "/css",
+    element: <Css/>,
+  },
+  {
+    path: "/javascript",
+    element: <JavaScript/>,
+  },
+
+  
+]);
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+  <RouterProvider router={router} />
   </React.StrictMode>
 );
 
